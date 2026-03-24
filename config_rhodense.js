ab585d25637adb2c04c07102a43010a8ba2db796
depmatteo2-max <depmatteo2@gmail.com>
2026-03-24 23:53:06 +0100

    Add files via upload

---
+++ config_rhodense.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+  SUPABASE_URL: 'https://hvakazxnvooffskvoyyl.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2YWthenhudm9vZmZza3ZveXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NDQ0NDYsImV4cCI6MjA4OTQyMDQ0Nn0.LfEYuBmb-4f2TuuY1rUGn1SSebMxYN8TNWL4FrfBUZw',
+  CLIENTE: 'rhodense',
+  USERS: [
+    { username: 'admin1', password: 'rhondense2026@', ruolo: 'admin', nome: 'Admin 1' },
+    { username: 'admin2', password: 'rhondense2026@', ruolo: 'admin', nome: 'Admin 2' },
+    { username: 'admin3', password: 'rhondense2026@', ruolo: 'admin', nome: 'Admin 3' },
+    { username: 'arbitro1', password: 'Arbitro1!', ruolo: 'arbitro', nome: 'Arbitro 1' },
+    { username: 'arbitro2', password: 'Arbitro2!', ruolo: 'arbitro', nome: 'Arbitro 2' },
+    { username: 'arbitro3', password: 'Arbitro3!', ruolo: 'arbitro', nome: 'Arbitro 3' },
+  ],
+  ADMIN_PASSWORD: 'rhondense2026@',
+};
