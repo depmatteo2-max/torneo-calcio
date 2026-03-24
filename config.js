beffcfd17553c5c6fc0a77edc1bbf265ff44ab21
depmatteo2-max <depmatteo2@gmail.com>
2026-03-24 23:14:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   SUPABASE_URL: 'https://hvakazxnvooffskvoyyl.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_JRJmEOL1ne439tC3vzaqyw_H9CZwlIZ',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2YWthenhudm9vZmZza3ZveXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NDQ0NDYsImV4cCI6MjA4OTQyMDQ0Nn0.LfEYuBmb-4f2TuuY1rUGn1SSebMxYN8TNWL4FrfBUZw',
   USERS: [
     { username: 'admin1', password: 'rhondense2026@', ruolo: 'admin', nome: 'Admin 1' },
     { username: 'admin2', password: 'rhondense2026@', ruolo: 'admin', nome: 'Admin 2' },
