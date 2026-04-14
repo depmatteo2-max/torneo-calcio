6e059a549ec93e1022ca1b5a6ebdbb34b5a7d022
depmatteo2-max <depmatteo2@gmail.com>
2026-04-14 10:43:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1393,8 +1393,7 @@ async function renderAdminLoghi() {
  if (!s) return '';
  let r = s.toLowerCase()
  .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
- .replace(/[‘’`]/g, '')
- .replace(PREFISSI, '')
+  .replace(PREFISSI, '')
  .replace(SUFFISSI, '')
  .replace(/[^a-z0-9\s]/g, ' ')
  .replace(/\s+/g, ' ')
