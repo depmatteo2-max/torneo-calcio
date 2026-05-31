b16716f818bc0b828cd5055eb3717a8af868a33a
depmatteo2-max <depmatteo2@gmail.com>
2026-05-31 10:13:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 //  Modifica SUPABASE_URL e SUPABASE_ANON_KEY con le tue credenziali
 // ============================================================
 const CONFIG = {
-  SUPABASE_URL     : 'https://XXXXXX.supabase.co',          // ← inserisci la tua URL Supabase
-  SUPABASE_ANON_KEY: 'eyXXXXXX',                             // ← inserisci la tua anon key
+  SUPABASE_URL     : 'https://hvakazxnvooffskvoyyl.supabase.co',          // ← inserisci la tua URL Supabase
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2YWthenhudm9vZmZza3ZveXlsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Mzg0NDQ0NiwiZXhwIjoyMDg5NDIwNDQ2fQ.nvVxQFCO8Bs_8A1AaSTe4xnFe_6cBX2w65kt1kNSS3w',                             // ← inserisci la tua anon key
   CLIENTE          : 'mclion',                               // identificatore univoco nel DB
   NOME_SITO        : '🦁 MC Lion Trophy 2026',
   USERS: [
